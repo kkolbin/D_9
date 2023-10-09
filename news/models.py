e402c9712f6d6fa7155a60a92133601b372960fa
kkolbin <126053132+kkolbin@users.noreply.github.com>
2023-10-09 17:16:54 +0900

    Update models.py

--- news/models.py
+++ news/models.py
@@ -13,39 +13,11 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField()
 
-    def send_weekly_notification(self):
-        # Logic to fetch new articles published by this author within the last week
-        new_articles = Post.objects.filter(author=self.user,
-                                           created_at__gte=timezone.now() - timezone.timedelta(days=7))
-
-        if new_articles:
-            email_subject = 'Your Weekly Article Summary'
-            email_content = 'Here are your new articles published in the last week:\n'
-            for article in new_articles:
-                article_url = article.get_absolute_url()
-                email_content += f'- {article.title}: {article_url}\n'
-
-            send_mail(email_subject, email_content, 'from@example.com', [self.user.email])
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     subscribers = models.ManyToManyField(User, related_name='subscribed_categories', blank=True)
     posts = models.ManyToManyField('Post', through='PostCategory')
-
-    def send_weekly_notification(self):
-        # Logic to fetch new articles in this category within the last week
-        new_articles = Post.objects.filter(categories=self, created_at__gte=timezone.now() - timezone.timedelta(days=7))
-
-        for user in self.subscribers.all():
-            if new_articles:
-                email_subject = f'New Articles in {self.name} Category'
-                email_content = f'Here are the new articles in {self.name} category published in the last week:\n'
-                for article in new_articles:
-                    article_url = article.get_absolute_url()
-                    email_content += f'- {article.title}: {article_url}\n'
-
-                send_mail(email_subject, email_content, 'from@example.com', [user.email])
 
     def __str__(self):
         return self.name
@@ -73,9 +45,10 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if self.post_type == 'news' and not self.created_at:
             # Check the number of news articles published by the user today
-            today_news_count = Post.objects.filter(author=self.author, post_type='news', created_at__date=timezone.now().date()).count()
+            today_news_count = Post.objects.filter(author=self.author, post_type='news',
+                                                   created_at__date=timezone.now().date()).count()
             if today_news_count >= 3:
-                raise ValidationError("A user can't publish more than three news articles in a day.")
+                raise ValidationError("Пользователь не может публиковать больше трёх статей в день.")
 
             self.created_at = timezone.now()
 
@@ -103,8 +76,4 @@ def add_user_to_common_group(sender, instance, created, **kwargs):
         instance.groups.add(common_group)
 
 
-class LastWeeklyNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    last_notification_date = models.DateTimeField()
-    objects = models.Manager()
 
